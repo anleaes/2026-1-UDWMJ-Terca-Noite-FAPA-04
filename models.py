2254b806b533fb6d999101d878be55f3c63cacac
Mxtths <m4theusps2004@gmail.com>
2026-06-07 20:15:40 -0300

    1st-item-models

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from category.models import Category
+
+class Item(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    fabrication_date = models.DateField('Data Fabricacao', auto_now=False, auto_now_add=False) 
+    is_active = models.BooleanField('Ativo', default=False)
+    price = models.DecimalField('Preço', max_digits=10, decimal_places=2, default=0)
+    photo = models.ImageField('Foto', upload_to='photos')
+    doc = models.FileField('Documentos', upload_to='docs')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    class Meta: 
+        verbose_name = 'item'      
+        verbose_name_plural = 'itens'
+        ordering =['id'] 
