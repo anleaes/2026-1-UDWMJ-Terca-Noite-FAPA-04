ea7c79c55e157011b0e9c09b29d7b71885a7250f
Gabriel Farias <161659894+Gabrieldrf@users.noreply.github.com>
2026-06-08 12:02:14 -0300

    13-commit

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'auctionitem'
+
+urlpatterns = [
+    path('', views.list_auctionitems, name='list_auctionitems'),
+    path('lances/', views.bids, name='bids'),
+    path('lances/adicionar/<int:item_id>/', views.add_bid, name='add_bid'),
+    path('lances/editar/<int:item_id>/', views.edit_bid, name='edit_bid'),
+    path('lances/excluir/<int:item_id>/', views.delete_bid, name='delete_bid'),
+    path('checkout/', views.checkout, name='checkout'),
+]
