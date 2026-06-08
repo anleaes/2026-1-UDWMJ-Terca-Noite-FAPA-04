b6bf93da3f83db50bc1336a8f8e0f49f2378b9bc
Gabriel Farias <161659894+Gabrieldrf@users.noreply.github.com>
2026-06-08 11:49:10 -0300

    9-commit

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from person.models import Person
+from contactchannel.models import Contactchannel
+
+class Auctioneer(Person):
+    company = models.CharField('Empresa', max_length=100)
+    evaluation = models.FloatField('Avaliacao',null=True, blank=True, default=0.0)
+    commission = models.FloatField('Comissao',null=True, blank=True, default=0.0)
+    status = models.BooleanField('Status', default=True)
+
+    class Meta:
+        verbose_name = 'Leiloeiro'
+        verbose_name_plural = 'Leiloeiros'
+        ordering =['id']
+
+    def __str__(self):
+        return super().first_name
