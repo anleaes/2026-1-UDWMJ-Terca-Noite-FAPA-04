b148e1e01bdf1816281465d1f4523e6d4f75cd7c
Mxtths <m4theusps2004@gmail.com>
2026-06-08 23:52:20 -0300

    2st-receipt-models

--- apps/receiptpayment/models.py
+++ apps/receiptpayment/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from auction.models import Auction
 from payment.models import Payment
 
+class Receipt(models.Model):
+    number = models.CharField('Número', max_length=100, unique=True)
+    issue_date = models.DateField('Data de emissão', auto_now_add=True)
+    auction = models.OneToOneField(Auction, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Recibo'
+        verbose_name_plural = 'Recibos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.number
