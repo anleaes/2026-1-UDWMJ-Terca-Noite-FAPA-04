bc29387d4332f38edac43532e03d7823c33c3ddb
Mxtths <m4theusps2004@gmail.com>
2026-06-09 00:09:16 -0300

    4st-item-models

--- apps/item/models.py
+++ apps/item/models.py
@@ -5,16 +5,14 @@ class Item(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
     fabrication_date = models.DateField('Data Fabricacao', auto_now=False, auto_now_add=False) 
-    is_active = models.BooleanField('Ativo', default=False)
     price = models.DecimalField('Preço', max_digits=10, decimal_places=2, default=0)
     photo = models.ImageField('Foto', upload_to='photos')
-    doc = models.FileField('Documentos', upload_to='docs')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
-    class Meta: 
-        verbose_name = 'item'      
-        verbose_name_plural = 'itens'
-        ordering =['id'] 
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Itens'
+        ordering =['id']
 
     def __str__(self):
-        return self.name 
+        return self.name
